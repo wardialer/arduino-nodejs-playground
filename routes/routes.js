01e7956084ab01fc41b986c0fc6a556e8dbc10d0
giravedu <giuseppe.raveduto@eng.it>
2015-07-20 23:07:33 +0200

    removed unused code; removed console log; just return celsius temperature instead of the entire json

--- routes/routes.js
+++ routes/routes.js
@@ -10,18 +10,5 @@ board.on("ready", function() {
 });
 
 exports.test = function(req, res, next){
-    console.log(temp);
-    res.json(temp);
+    res.json(temp.celsius);
 };
-
-/*
-board.on("ready", function() {
-  var temp = new five.Temperature({
-    pin: "A0",
-    controller: "LM35"
-  });
-
-  temp.on("change", function() {
-    console.log("Temp: %d", this.celsius);
-  });
-});*/
