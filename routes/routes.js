de395ed561859473bc99bad31dbca6df57d58ad7
giravedu <giuseppe.raveduto@eng.it>
2015-07-25 10:36:20 +0200

    added scaling for sensors

--- routes/routes.js
+++ routes/routes.js
@@ -7,20 +7,31 @@ board.on("ready", function() {
         pin: "A0",
         controller: "LM35"
     });
+
     light = new five.Sensor({
         pin: "A1",
         freq: 250
-    });
+    }).scale(0, 100);
+
     humidity = new five.Sensor({
         pin: "A2",
         freq: 250
-    });
+    }).scale(0, 100);
 
 });
 
 exports.test = function(req, res, next){
-    var obj = {temp: temp.celsius, 
-               light: light.value,
-               humidity: humidity.value};
+    var obj = {
+        temp: temp.celsius, 
+        light: {
+            scaled: light.value, 
+            raw: light.raw
+        },
+        humidity: {
+            scaled: humidity.value, 
+            raw: humidity.raw
+        }
+   };
+   
     res.json(obj);
 };
