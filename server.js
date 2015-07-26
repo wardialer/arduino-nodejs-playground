19759e27bc583b3b3ca5efc4b30d3dbeeb0b807d
giravedu <giuseppe.raveduto@eng.it>
2015-07-26 13:30:32 +0200

    added mongoose 4.0.1

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var routes = require('./routes/routes');
 var http = require('http');
 var path = require('path');
+var mongoose = require('mongoose');
 
 var app = express();
 
 // all environments
+mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds063892.mongolab.com:63892/friendly-killer-robot");
 app.set('port', process.env.PORT || 3000);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
