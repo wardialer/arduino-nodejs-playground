32442290ea68fd3fa1ce1b7096bcd3803fa8920e
denisa <denisa.ziu@eng.it>
2015-07-28 23:43:28 +0200

    bugfix: import

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var routes = require('./routes/routes');
 var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
-var Reading = require('../models/reading');
+var Reading = require('models/reading');
 var repeat = require('repeat');
 
 var app = express();
@@ -34,3 +34,4 @@ var readSensors = function(){
 }
 
 repeat(readSensors).every(1,'m').start.now();
+
