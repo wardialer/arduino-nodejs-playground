a5e377c9bdecbd893ca4d0d0ba4be3a9d0affee4
giravedu <giuseppe.raveduto@eng.it>
2015-08-02 17:09:37 +0200

    test data

--- public/js/controllers/ChartsCtrl.js
+++ public/js/controllers/ChartsCtrl.js
@@ -4,20 +4,19 @@ angular.module('ChartsCtrl', [])
     googleChartApiPromise.then(function() {
         var countersChart = new google.visualization.DataTable();
         countersChart.addColumn("string", "date");
-        countersChart.addColumn("string", "date");
         countersChart.addColumn("number", "temperature");
         countersChart.addColumn("number", "humidity");
         countersChart.addColumn("number", "light");
 
         countersChart.addRows(2);
-        countersChart.setCell(0, 1, '08/05/1985');
-        countersChart.setCell(0, 2, 180);
+        countersChart.setCell(0, 0, '08/05/1985');
+        countersChart.setCell(0, 1, 180);
+        countersChart.setCell(0, 2, 30);
         countersChart.setCell(0, 3, 30);
-        countersChart.setCell(0, 4, 30);
-        countersChart.setCell(1, 1, '07/07/1986');
-        countersChart.setCell(1, 2, 180);
+        countersChart.setCell(1, 0, '07/07/1986');
+        countersChart.setCell(1, 1, 180);
+        countersChart.setCell(1, 2, 30);
         countersChart.setCell(1, 3, 30);
-        countersChart.setCell(1, 4, 30);
 
         $scope.countersChart = {
             type: 'LineChart',
