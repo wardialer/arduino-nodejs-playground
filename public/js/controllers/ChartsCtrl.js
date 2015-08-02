d94f407cb5bdf11fe2bdd469be5dd9495c4e03ad
giravedu <giuseppe.raveduto@eng.it>
2015-08-02 17:34:52 +0200

    reading real data from sensors

--- public/js/controllers/ChartsCtrl.js
+++ public/js/controllers/ChartsCtrl.js
@@ -1,29 +1,33 @@
 angular.module('ChartsCtrl', [])
 .controller('ChartsController', ['$scope', '$http', 'googleChartApiPromise', function($scope, $http, googleChartApiPromise) {
 
-    googleChartApiPromise.then(function() {
-        var countersChart = new google.visualization.DataTable();
-        countersChart.addColumn("string", "date");
-        countersChart.addColumn("number", "temperature");
-        countersChart.addColumn("number", "humidity");
-        countersChart.addColumn("number", "light");
+    $http.get('/get').success(function(readings) {
 
-        countersChart.addRows(2);
-        countersChart.setCell(0, 0, '08/05/1985');
-        countersChart.setCell(0, 1, 180);
-        countersChart.setCell(0, 2, 30);
-        countersChart.setCell(0, 3, 30);
-        countersChart.setCell(1, 0, '07/07/1986');
-        countersChart.setCell(1, 1, 180);
-        countersChart.setCell(1, 2, 30);
-        countersChart.setCell(1, 3, 30);
+        googleChartApiPromise.then(function() {
+            var countersChart = new google.visualization.DataTable();
+            countersChart.addColumn("string", "date");
+            countersChart.addColumn("number", "temperature");
+            countersChart.addColumn("number", "humidity");
+            countersChart.addColumn("number", "light");
 
-        $scope.countersChart = {
-            type: 'LineChart',
-            data: countersChart,
-            options: {backgroundColor: '#FAFAFA', height: 500, legend: {position: 'top', maxLines: 3}, chartArea: {left: 0, width: '100%', height: '70%'}}
-        };
+            countersChart.addRows(readings.length);
+            for (var i=0; i<readings.length; i++) {
+                var reading = readings[i];
 
+                countersChart.setCell(i, 0, reading.date);
+                countersChart.setCell(i, 1, reading.temp);
+                countersChart.setCell(i, 2, reading.humidity.raw);
+                countersChart.setCell(i, 3, reading.light.raw);
+            }
+
+            $scope.countersChart = {
+                type: 'LineChart',
+                data: countersChart,
+                options: {backgroundColor: '#FAFAFA', height: 500, legend: {position: 'top', maxLines: 3}, chartArea: {left: 0, width: '100%', height: '70%'}}
+            };
+
+        });
     });
+
 
 }]);
