6e33c1d5de50f15e4ec7fa4d6145a768562dc6e5
giravedu <giuseppe.raveduto@eng.it>
2015-08-02 17:58:33 +0200

    sort readings by date

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,8 @@
 var Reading = require('../models/reading');
 
 exports.get = function(req, res, next){
-    Reading.find().exec(function(err, readings) {
+    Reading.find().sort({date: 1})
+    .exec(function(err, readings) {
         res.json(readings);
     });  
 };
