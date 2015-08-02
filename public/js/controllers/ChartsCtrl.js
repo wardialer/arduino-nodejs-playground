cbfbd45998656488ecf85f65ea0a27f213955d73
giravedu <giuseppe.raveduto@eng.it>
2015-08-02 18:19:34 +0200

    formatted chart

--- public/js/controllers/ChartsCtrl.js
+++ public/js/controllers/ChartsCtrl.js
@@ -5,26 +5,35 @@ angular.module('ChartsCtrl', [])
 
         googleChartApiPromise.then(function() {
             var countersChart = new google.visualization.DataTable();
-            countersChart.addColumn("string", "date");
-            countersChart.addColumn("number", "temperature");
-            countersChart.addColumn("number", "humidity");
-            countersChart.addColumn("number", "light");
+            countersChart.addColumn("date", "Date");
+            countersChart.addColumn("number", "Temperature (°C)");
+            countersChart.addColumn("number", "Humidity (scaled)");
+            countersChart.addColumn("number", "Light (scaled)");
 
             countersChart.addRows(readings.length);
             for (var i=0; i<readings.length; i++) {
                 var reading = readings[i];
 
-                countersChart.setCell(i, 0, reading.date);
+                countersChart.setCell(i, 0, new Date(reading.date));
                 countersChart.setCell(i, 1, reading.temp);
-                countersChart.setCell(i, 2, reading.humidity.raw);
-                countersChart.setCell(i, 3, reading.light.raw);
+                countersChart.setCell(i, 2, reading.humidity.scaled);
+                countersChart.setCell(i, 3, reading.light.scaled);
             }
 
             $scope.countersChart = {
                 type: 'LineChart',
                 data: countersChart,
                 options: {
+                    title: 'Sensors'
                     height: 500, 
+                    hAxis: {
+                        format: 'd/M/yy',
+                        gridlines: {count: 15}
+                    },
+                    vAxis: {
+                        gridlines: {color: 'none'},
+                        minValue: 0
+                    }
                     legend: {
                         position: 'top', 
                         maxLines: 3
