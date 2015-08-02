2c875c8ff9d57b35f517106e785390f42b7ca62c
giravedu <giuseppe.raveduto@eng.it>
2015-08-02 17:07:40 +0200

    test data

--- public/js/controllers/ChartsCtrl.js
+++ public/js/controllers/ChartsCtrl.js
@@ -3,18 +3,23 @@ angular.module('ChartsCtrl', [])
 
     googleChartApiPromise.then(function() {
         var countersChart = new google.visualization.DataTable();
-        countersChart.addColumn("string", "label");
-        countersChart.addColumn("number", "elementi totali");
-        countersChart.addColumn("number", "geolocalizzati");
-        countersChart.addColumn("number", "multimediali");
+        countersChart.addColumn("string", "date");
+        countersChart.addColumn("number", "temperature");
+        countersChart.addColumn("number", "humidity");
+        countersChart.addColumn("number", "light");
 
-        countersChart.addRows(1);
-        countersChart.setCell(0, 1, 150);
+        countersChart.addRows(2);
+        countersChart.setCell(0, 1, '08/05/1985');
         countersChart.setCell(0, 2, 180);
         countersChart.setCell(0, 3, 30);
+        countersChart.setCell(0, 4, 30);
+        countersChart.setCell(1, 1, '07/07/1986');
+        countersChart.setCell(1, 2, 180);
+        countersChart.setCell(1, 3, 30);
+        countersChart.setCell(1, 4, 30);
 
         $scope.countersChart = {
-            type: 'BarChart',
+            type: 'LineChart',
             data: countersChart,
             options: {backgroundColor: '#FAFAFA', height: 500, legend: {position: 'top', maxLines: 3}, chartArea: {left: 0, width: '100%', height: '70%'}}
         };
