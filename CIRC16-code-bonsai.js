42f6059fe5fe77af7bcea1a75c4cebdb9adeaae8
giravedu <giuseppe.raveduto@eng.it>
2015-11-01 22:46:51 +0100

    test sensor change listener

--- CIRC16-code-bonsai.js
+++ CIRC16-code-bonsai.js
@@ -6,13 +6,14 @@ board.on("ready", function() {
 var humidity = new five.Sensor({
         	pin: "A0",
 	        freq: 250
-	    })
+	    }).on('change', function() {
+            console.log(this.value);
+        })
 
     var pin = new five.Pin(8);
     pin.low();
     this.loop(500, function() {
 	   var status = humidity.value;
-        console.log(status);
 	   if (status > 300) pin.high();
 	   else pin.low();
     });
