0b429d5cb96a68a98ae7a08d1fa4bb7582d86ee9
giravedu <giuseppe.raveduto@eng.it>
2015-11-01 23:22:46 +0100

    ops...

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ var humidity = new five.Sensor({
         .on('change', function() {
             var value = this.value;
 
-            if (value-prev <= 5 || prev-value <= 5) return;
+            if ((value-prev <= 5 || prev-value <= 5) && prev!=0) return;
 
             prev = value;
             saveSensorData(this);
