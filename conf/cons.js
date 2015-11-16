b4eabd42944da12e30b4c032bd8cfa02f6b43dd5
denisa <denisa.ziu@eng.it>
2015-11-16 23:16:20 +0100

    commited changes

---
+++ conf/cons.js
@@ -0,0 +1,10 @@
+exports.relayPin = 8;
+exports.temperaturePin = "A4";
+exports.humidityPin = "A5";
+exports.humidityFrequency = 250;
+exports.temperatureController = "LM35";
+exports.sensorNames = {
+    humidity: "humidity",
+    temperature: "temperature",
+    light: "light"
+}
