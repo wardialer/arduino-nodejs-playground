08be9c9b0b66221d59e2216d723e1be42f6188d9
denisa <denisa.ziu@eng.it>
2015-11-16 23:04:14 +0100

    fix pathname

--- conf/config.js
+++ conf/config.js
@@ -1,4 +1,4 @@
-exports.username = 'username';
-exports.password = 'password';
-exports.ids = ['id1','id2'];
-exports.botKey = 'botKey';
+module.exports.username = 'username';
+module.exports.password = 'password';
+module.exports.ids = ['id1','id2'];
+module.exports.botKey = 'botKey';
